cb9b291481ecc72f658a0afef80fc0895a8463f4
lijinwei <lijinwei@hztianque.com>
2019-10-04 21:07:08 +0800

    springboot03--jackJosn测试

--- src/main/java/com/liyu/helloworld/domain/Animal.java
+++ src/main/java/com/liyu/helloworld/domain/Animal.java
@@ -1,5 +1,9 @@
 package com.liyu.helloworld.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,9 +21,14 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonPropertyOrder(value={"id","type"})//更换变量显示顺序
 public class Animal {
+        @JsonIgnore      //不支持序列化，不能传参
         private  String name;
+        @JsonProperty("type1")
         private  Integer type;
+        //空值不返回
+        @JsonInclude(JsonInclude.Include.NON_NULL)
         private  String num;
         private  Long id;
         private Date birthDate;

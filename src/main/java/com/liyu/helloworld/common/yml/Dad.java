3e42e812b4822d93d6423e3bc911756a14a93965
lijinwei <lijinwei@hztianque.com>
2019-10-19 21:04:55 +0800

    springboot05-2-数据验证

--- src/main/java/com/liyu/helloworld/common/yml/Dad.java
+++ src/main/java/com/liyu/helloworld/common/yml/Dad.java
@@ -2,6 +2,8 @@ package com.liyu.helloworld.common.yml;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+
 /**
  * @author Levi
  * @date 2019/10/7 14:09
@@ -9,7 +11,7 @@ import lombok.Data;
 @Data
 public class Dad {
     private String name;
-
+    //@Min(50)
     private  int age;
 
 }
--- src/main/java/com/liyu/helloworld/common/yml/Family.java
+++ src/main/java/com/liyu/helloworld/common/yml/Family.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * @author Levi
@@ -11,9 +14,11 @@ import org.springframework.stereotype.Component;
  */
 @Data
 @Component
+@Validated
 @ConfigurationProperties(prefix = "family")
 public class Family {
     //@Value("${family.family-name}")
+    @NotNull(message = "familyName不能为空")
     private String familyName;
     private Dad dad;
     private Mom mom;
